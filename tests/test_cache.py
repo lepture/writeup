5c2e2cde3b7e005ec2706bff8f11f0b3a53015a0
Hsiaoming Yang <me@lepture.com>
2013-11-08 11:57:59 +0800

    Add cache system.

---
+++ tests/test_cache.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+from writeup.cache import Cache
+import tempfile
+
+
+def test_memcache():
+    cache = Cache()
+    assert cache.get('foo') is None
+    assert cache.mtime('foo') is None
+
+    cache.set('foo', 'foo')
+    assert cache.has('foo')
+    assert cache.mtime('foo') is not None
+
+    assert cache.get('foo') == 'foo'
+
+    cache.set('bar', 'bar')
+    cache.set('baz', 'baz')
+
+    assert cache.has('bar')
+    cache.clear('bar')
+    assert not cache.has('bar')
+
+    cache.flush()
+    assert not cache.has('baz')
+    assert not cache.has('foo')
+
+
+def test_filecache():
+    cachedir = tempfile.mkdtemp()
+    cache = Cache(cachedir)
+    assert cache.get('foo') is None
+    assert cache.mtime('foo') is None
+
+    cache.set('foo', 'foo')
+    assert cache.has('foo')
+    assert cache.mtime('foo') is not None
+
+    assert cache.get('foo') == 'foo'
+
+    cache.set('bar', 'bar')
+    cache.set('baz', 'baz')
+
+    assert cache.has('bar')
+    cache.clear('bar')
+    assert not cache.has('bar')
+
+    cache.flush()
+    assert not cache.has('baz')
+    assert not cache.has('foo')
