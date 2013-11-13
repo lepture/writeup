e563cb1e794e67db1cf70a27087c09d70dfc1104
Hsiaoming Yang <me@lepture.com>
2013-11-14 02:28:25 +0800

    More tests

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -33,3 +33,27 @@ def test_highlight_markdown():
 def test_highlight_markdown():
     html = filters.markdown(text, highlight=True, inlinestyles=True)
     assert 'background:' in html
+
+
+def test_youtube_link():
+    html = filters.markdown(
+        'hello http://www.youtube.com/watch?v=p8Ey0AufD9g'
+    )
+    assert 'iframe' in html
+
+
+def test_youtube_link2():
+    html = filters.markdown('hello http://youtu.be/p8Ey0AufD9g')
+    assert 'iframe' in html
+
+
+def test_gist_link():
+    html = filters.markdown(
+        'hello https://gist.github.com/paulmillr/2657075'
+    )
+    assert 'script' in html
+
+
+def test_vimeo_link():
+    html = filters.markdown('hello https://vimeo.com/79148964')
+    assert 'iframe' in html
