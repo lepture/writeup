63013fc9fb9c5395556321fb273d34abd5afa14c
Hsiaoming Yang <me@lepture.com>
2013-12-01 17:17:47 +0800

    Server can return 304 now.

--- writeup/server.py
+++ writeup/server.py
@@ -9,16 +9,20 @@
 """
 
 import os
+import hashlib
+import datetime
 import mimetypes
+import email.utils
 from wsgiref.simple_server import make_server
+from . import __version__
 
 
 class Server(object):
     def __init__(self, sitedir='_site'):
         self._sitedir = sitedir
 
-    def read(self, url):
-        """Reading from a file."""
+    def filepath(self, url):
+        """Parse the real filepath of a url."""
         url = url.lstrip('/')
         url = os.path.join(self._sitedir, url)
 
@@ -29,9 +33,22 @@ class Server(object):
 
         if not os.path.exists(url):
             return None
+        return url
 
+    def read(self, url):
+        """Reading from a file."""
+        url = self.filepath(url)
+        if not url:
+            return None
         with open(url, 'r') as f:
             return f.read()
+
+    def mtime(self, url):
+        url = self.filepath(url)
+        if not url:
+            return None
+        mtime = os.path.getmtime(url)
+        return email.utils.formatdate(mtime, usegmt=True)
 
     def serve_forever(self, host='0.0.0.0', port=4000):
         print('Start server at http://%s:%s' % (host, port))
@@ -44,12 +61,26 @@ class Server(object):
             mime_types = 'text/html'
 
         body = self.read(path)
-        headers = [('Content-Type', mime_types)]
+        headers = [
+            ('Content-Type', mime_types),
+            ('Server', 'Writeup/%s' % __version__),
+        ]
         if body is None:
             start_response('404 Not Found', headers)
             not_found = self.read('404.html')
             if not_found:
                 yield not_found
         else:
-            start_response('200 OK', headers)
-            yield body
+            etag = hashlib.md5(body).hexdigest()
+            match = environ.get('HTTP_IF_NONE_MATCH', None)
+
+            mtime = self.mtime(path)
+            modified = environ.get('HTTP_IF_MODIFIED_SINCE', None)
+            if etag == match or modified == mtime:
+                start_response('304 Not Modified', headers)
+            else:
+                headers.append(('Etag', etag))
+                headers.append(('Last-Modified', mtime))
+                headers.append(('Content-Length', str(len(body))))
+                start_response('200 OK', headers)
+                yield body
