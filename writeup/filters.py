ed7f0d30d2913c24369a5d749d95a5d3d466d219
Hsiaoming Yang <me@lepture.com>
2013-12-06 13:18:49 +0800

    Video render for markdown.

--- writeup/filters.py
+++ writeup/filters.py
@@ -16,56 +16,130 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 
 
-class AutolinkRenderer(m.HtmlRenderer):
+def _iframe(src, width=650, height=365, content=None, link=None):
+    """Create an iframe html snippet."""
+    html = (
+        '<iframe width="%s" height="%s" src="%s" '
+        'frameborder="0" allowfullscreen></iframe>'
+    ) % (width, height, src)
+    if not content:
+        return html
+    if link:
+        content = '<a href="%s">%s</a>' % (link, content)
+    return '<figure>%s<figcaption>%s</figcaption></figure>' % (
+        html, content
+    )
+
+
+def youtube(link):
+    pattern = r'http://www\.youtube\.com\/watch\?v=([a-zA-Z0-9\-\_]+)'
+    match = re.match(pattern, link)
+    if not match:
+        pattern = r'http:\/\/youtu.be\/([a-zA-Z0-9\-\_]+)'
+        match = re.match(pattern, link)
+    if not match:
+        return None
+    return 'http://www.youtube.com/embed/%s' % match.group(1)
+
+
+def vimeo(link):
+    #: vimeo.com
+    pattern = r'https?:\/\/vimeo\.com\/([\d]+)'
+    match = re.match(pattern, link)
+    if not match:
+        return None
+    return 'https://player.vimeo.com/video/%s' % match.group(1)
+
+
+def youku(link):
+    #: youku.com
+    pattern = r'http:\/\/v\.youku\.com\/v_show/id_([\w]+)\.html'
+    match = re.match(pattern, link)
+    if not match:
+        return None
+    return 'http://player.youku.com/embed/%s' % match.group(1)
+
+
+def gist(link, content=None):
+    #: gist support
+    pattern = r'(https?:\/\/gist\.github\.com\/.+\d+)'
+    match = re.match(pattern, link)
+    if not match:
+        return None
+    html = '<script src="%(link)s.js"></script>' % match.group(1)
+    if not content:
+        return html
+    return '<figure>%s<figcaption>%s</figcaption></figure>' % (
+        html, content
+    )
+
+
+def embed(link, width=650, height=366, content=None):
+    html = gist(link, content)
+    if html:
+        return html
+
+    src = youtube(link)
+    if src:
+        return _iframe(src, width, height, content, link)
+    src = vimeo(link)
+    if src:
+        return _iframe(src, width, height, content, link)
+    src = youku(link)
+    if src:
+        return _iframe(src, width, height, content, link)
+    return None
+
+
+class BaseRenderer(m.HtmlRenderer):
     def autolink(self, link, is_email):
         if is_email:
             return '<a href="mailto:%(link)s">%(link)s</a>' % {'link': link}
-        title = link.replace('http://', '').replace('https://', '')
+        html = embed(link)
+        if html:
+            return html
+        content = link.replace('http://', '').replace('https://', '')
+        return '<a href="%s">%s</a>' % (link, content)
 
-        #: youtube.com
-        pattern = r'http://www\.youtube\.com\/watch\?v=([a-zA-Z0-9\-\_]+)'
-        match = re.match(pattern, link)
-        if not match:
-            pattern = r'http:\/\/youtu.be\/([a-zA-Z0-9\-\_]+)'
-            match = re.match(pattern, link)
-        if match:
-            value = (
-                '<iframe width="560" height="315" src='
-                '"http://www.youtube.com/embed/%(id)s" '
-                'frameborder="0" allowfullscreen></iframe>'
-                '<div><a rel="nofollow" href="%(link)s">'
-                '%(title)s</a></div>'
-            ) % {'id': match.group(1), 'link': link, 'title': title}
-            return value
+    def link(self, link, title, content):
+        width = 650
+        height = 366
+        if title:
+            # title can descibe height and width: 650 x 366
+            pattern = r'(\d{3,4})[^\d]+(\d{3})'
+            match = re.match(pattern, title)
+            if match:
+                width = match.group(1)
+                height = match.group(2)
+        html = embed(link, width, height, content)
+        if html:
+            return html
+        html = '<a href="%s"' % link
+        if title:
+            html = '%s title="%s"' % (html, title)
+        html = '%s>%s</a>' % (html, content)
+        return html
 
-        #: gist support
-        pattern = r'(https?:\/\/gist\.github\.com\/.+\d+)'
-        match = re.match(pattern, link)
-        if match:
-            value = (
-                '<script src="%(link)s.js"></script>'
-                '<div><a rel="nofollow" href="%(link)s">'
-                '%(title)s</a></div>'
-            ) % {'link': match.group(1), 'title': title}
-            return value
+    def image(self, link, title, alt_text):
+        html = '<img src="%s" alt="%s" />' % (link, alt_text)
+        if not title:
+            return html
+        return '<figure>%s<figcaption>%s</figcaption></figure>' % (
+            html, title
+        )
 
-        #: vimeo.com
-        pattern = r'https?:\/\/vimeo\.com\/([\d]+)'
-        match = re.match(pattern, link)
-        if match:
-            value = (
-                '<iframe width="566" height="318" frameborder="0" '
-                'src="https://player.vimeo.com/video/%(id)s" '
-                'allowFullScreen></iframe>'
-                '<div><a rel="nofollow" href="%(link)s">'
-                '%(title)s</a></div>'
-            ) % {'id': match.group(1), 'link': link, 'title': title}
-            return value
-        return '<a href="%(link)s">%(title)s</a>' % \
-                {'link': link, 'title': title}
+    def paragraph(self, content):
+        pattern = r'<figure>.*</figure>'
+        if re.match(pattern, content):
+            return content
+        # a single image in this paragraph
+        pattern = r'^<img[^>]+>$'
+        if re.match(pattern, content):
+            return '<figure>%s</figure>' % content
+        return '<p>%s</p>' % content
 
 
-class HighlightRenderer(AutolinkRenderer):
+class HighlightRenderer(BaseRenderer):
     def block_code(self, text, lang):
         if not lang:
             return u'<pre><code>%s</code></pre>' % escape(text)
@@ -107,7 +181,7 @@ def markdown(text, highlight=True, inlinestyles=False, linenos=False):
         renderer._inlinestyles = inlinestyles
         renderer._linenos = linenos
     else:
-        renderer = AutolinkRenderer()
+        renderer = BaseRenderer()
 
     extensions = (
         m.EXT_NO_INTRA_EMPHASIS | m.EXT_FENCED_CODE | m.EXT_AUTOLINK |
