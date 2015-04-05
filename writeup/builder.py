685cf5eeab37a26200e1b1c589cc9369856c2707
Hsiaoming Yang <me@lepture.com>
2015-04-05 21:56:16 +0800

    Design builder

--- writeup/builder.py
+++ writeup/builder.py
@@ -10,25 +10,78 @@
 """
 
 import os
-import json
 import logging
+from .request import Request
+from ._compat import to_unicode
 
 
 logger = logging.getLogger('writeup')
 
 
 class Builder(object):
-    def __init__(self):
-        pass
+    def __init__(self, app):
+        self.app = app
 
-    def is_supported(self, filename):
-        ext = os.path.splitext(filename)[1]
-        return ext in ('.md', '.mkd', '.markdown')
+    def get_destination(self, req):
+        dest = req.url
+        if dest.endswith('/'):
+            dest += 'index.html'
+        elif not dest.endswith('.html'):
+            dest += '.html'
+
+        dest = os.path.join(self.app.sitedir, dest.lstrip('/'))
+        if os.path.isfile(dest) and os.path.getmtime(dest) > req.mtime:
+            return None
+        return dest
+
+    @staticmethod
+    def write(content, dest):
+        # TODO
+        return
 
 
-class RequestBuilder(Builder):
-    pass
+class PostBuilder(Builder):
+    def build(self, filepath):
+        req = Request(filepath)
+        dest = self.get_destination(req)
+        if not dest:
+            return
+        template = req.template or 'post.html'
+        tpl = self.app.jinja.get_template(template)
+        content = tpl.render({'page': req})
+        self.write(content, dest)
+
+    def run(self):
+        for filepath in self.app.post_indexer.keys():
+            self.build(filepath)
 
 
-class HTMLBuilder(Builder):
-    pass
+class PageBuilder(PostBuilder):
+    def run(self):
+        for filepath in self.app.page_indexer.keys():
+            self.build(filepath)
+
+
+class FileBuilder(Builder):
+    def get_destination(self, filepath):
+        if self.app.postsdir in filepath:
+            dest = os.path.relpath(filepath, self.app.postsdir)
+        else:
+            dest = os.path.relpath(filepath, self.app.basedir)
+        return os.path.join(self.app.sitedir, dest)
+
+    def build_html(self, filepath):
+        dest = self.get_destination(filepath)
+        with open(filepath, 'rb') as f:
+            tpl = self.app.jinja.from_string(to_unicode(f.read()))
+        content = tpl.render()
+        self.write(content, dest)
+
+    def run(self):
+        for filepath in self.app.file_indexer.keys():
+            if filepath.endswith('/index.html'):
+                self.build_paginator(filepath)
+            elif filepath.endswith('.html') or filepath.endswith('.xml'):
+                self.build_html(filepath)
+            else:
+                self.build_asset(filepath)
