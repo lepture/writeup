a30865ca80aa7570f9eb52ac9e3f22a1099d8b27
Hsiaoming Yang <me@lepture.com>
2015-04-05 16:23:00 +0800

    Fix proxy

--- writeup/globals.py
+++ writeup/globals.py
@@ -12,12 +12,20 @@ class _Proxy(object):
 
     @cached_property
     def _obj(self):
-        return getattr(_top, self._ident)
+        return object.__getattribute__(_top, self._ident)
 
     def __getattr__(self, name):
         if name == '__members__':
             return dir(self._obj)
-        return getattr(self._obj, name)
+        return object.__getattribute__(self._obj, name)
+
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            return super(_Proxy, self).__setattr__(name, value)
+        return setattr(self._obj, name, value)
+
+    def __delattr__(self, name):
+        return delattr(self._obj, name)
 
     def __setitem__(self, key, value):
         self._obj[key] = value
@@ -25,8 +33,6 @@ class _Proxy(object):
     def __delitem__(self, key):
         del self._obj[key]
 
-    __setattr__ = lambda x, n, v: setattr(x._obj, n, v)
-    __delattr__ = lambda x, n: delattr(x._obj, n)
     __str__ = lambda x: str(x._obj)
     __repr__ = lambda x: repr(x._obj)
     __getitem__ = lambda x, i: x._obj[i]
